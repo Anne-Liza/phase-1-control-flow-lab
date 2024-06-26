0b1d19c7088a8f7fe758b041edf3f722414addf1
Anne <annewangui210@gmail.com>
2024-06-26 15:06:55 +0300

    if, else, switch functions.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+  if (feet <= 400){
+     result="This one is on me!"
+     return result;
+    }
+    else if (feet>400 && feet<2000){
+      result="That will be twenty bucks."
+      return result;
+    }
+    else if (feet>2000 && feet<=2500){
+      result="I will gladly take your thirty bucks."
+      return result;
+    }
+    else if(feet>=2500){
+      result="No can do."
+      return result;
+    }
+  }
+  
+    
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+    const result =(city==="NYC")? "Ok, sounds good." : "No go.";
+    return result;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
 }
